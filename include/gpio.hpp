017383953e8883ff50f95a0e6de7c83bf71ddb58
Ethan Rad <116464105+E-Rad1@users.noreply.github.com>
2024-12-07 20:27:37 -0800

    add gpio pin function

---
+++ include/gpio.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <Arduino.h>
+#include <Arduino_FreeRTOS.h>
+#include <semphr.h>
+#include "buffer.hpp"
+
+constexpr uint32_t gpio_buffer_size{1024};
+
+struct gpio_output_t {
+  buffer_t<uint8_t, gpio_buffer_size> gpio_data{};
+  SemaphoreHandle_t data_sem{NULL};
+};
+
+typedef struct gpio_config {
+    bool enabled{false}; 
+    uint32_t frequency;
+};
+
+gpio_output_t gpio_outputs[3];
+
+bool set_gpio_config(const int32_t num, const gpio_config gc);
+
+void gpio_task(void *pvParameters);
